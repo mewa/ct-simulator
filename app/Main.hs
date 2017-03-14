f35462338879e37115fa445d97594030312865c1
mewa <marcin.k.chmiel@gmail.com>
2017-03-14 20:06:48 +0100

    Changed library function to processImage and added arguments so it is ghci-usable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Lib
+import System.IO
+import System.Environment
 
 main :: IO ()
-main = someFunc
+main = do 
+    args <- getArgs
+    let fname = head args
+        angle = read $ args !! 1 :: Double
+    processImage fname angle
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,31 @@
 module Lib
-    ( someFunc
+    ( processImage
     ) where
 
 import Codec.Picture
 import Data.Either
 import System.Directory
 import System.IO.Unsafe
+import System.Environment
+import Debug.Trace
+import System.IO
+import qualified Data.Matrix as Mat
 
-someFunc :: IO ()
-someFunc = do
-  home <- getHomeDirectory
-  fname <- fmap head getArgs
+processImage :: String -> Double -> IO ()
+processImage fname angle = do
   a <- readImage $ fname
   case a of
     Left err -> putStrLn err
     Right img -> do
       putStrLn "Image Loaded"
-      print $ analyzeImage $ convertRGB8 img
+      let grey = pixelMap rgbToGreyscale $ convertRGB8 img
+          result = decompose grey angle
+          w = length $ result !! 0
+          h = length result
+          renderer x y = PixelYA8 (truncate $ result !! y !! x) 255
+      print $ "saving " ++ show w ++ "x" ++ show h
+      print "--OK"
+      writePng "res/result.png" $ generateImage renderer w h
   putStrLn "someFunc: "
 
 analyzeImage :: Image PixelRGB8 -> Int
